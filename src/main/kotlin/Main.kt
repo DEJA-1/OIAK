abdd8a7d27150e93774e47a1f738fca6e5ca7181
DEJA1 <allowestq@gmail.com>
2024-06-02 18:07:28 +0200

    add skippable conversion

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,17 +2,19 @@ import numberSystem.Decimal
 import numberSystem.base.NumberBaseSystem
 
 fun main() {
-    val value = 156124
-    for (base in 2..36) {
-        if (base == 10) continue
-        val decimalToBase = Decimal(value)
-            .convertToBase(base)
+//    val value = 156124
+//    for (base in 2..36) {
+//        if (base == 10) continue
+//        val decimalToBase = Decimal(value)
+//            .convertToBase(base)
+//
+//        decimalToBase.printValue()
+//
+//        decimalToBase.convertFromBaseToDecimal().printValue()
+//    }
+//
+//    val b34 = NumberBaseSystem("3X1U", 34)
+//    b34.convertFromBaseToBase(targetBase = 2).printValue()
 
-        decimalToBase.printValue()
-
-        decimalToBase.convertFromBaseToDecimal().printValue()
-    }
-
-    val b34 = NumberBaseSystem("3X1U", 34)
-    b34.convertFromBaseToBase(targetBase = 2).printValue()
+    NumberBaseSystem("1010111100", 2).convertFromBaseToBase(16).printValue()
 }
--- src/main/kotlin/numberSystem/base/NumberBaseSystem.kt
+++ src/main/kotlin/numberSystem/base/NumberBaseSystem.kt
@@ -2,25 +2,59 @@ package numberSystem.base
 
 import numberSystem.Decimal
 import numberSystem.NumberSystem
-import kotlin.math.pow
+import kotlin.math.*
 
 class NumberBaseSystem(val value: String, val base: Int) : NumberSystem<String>(value, base) {
     fun convertFromBaseToDecimal(): Decimal {
         require(base in 2..36) { "Base must be between 2 and 36" }
 
-        var result = 0
+        return Decimal(getSumOfCharSequence(sequence = value))
+    }
 
-        value.reversed().forEachIndexed { index, char ->
+    fun convertFromBaseToBase(targetBase: Int): NumberBaseSystem {
+        val doubleTargetBase = targetBase.toDouble()
+        var rootedTargetBase = sqrt(doubleTargetBase)
+
+        var skippable = false
+        while (rootedTargetBase > 1) {
+            rootedTargetBase = floor(sqrt(rootedTargetBase))
+            if (rootedTargetBase.toInt() == base) skippable = true
+        }
+
+        if (skippable) {
+            return convertFromBaseToBaseUsingExponent(targetBase)
+        }
+
+        val decimal = convertFromBaseToDecimal()
+        return decimal.convertToBase(targetBase)
+    }
+
+    private fun convertFromBaseToBaseUsingExponent(targetBase: Int): NumberBaseSystem {
+        val chunkSize = log(targetBase.toDouble(), base = base.toDouble()).toInt()
+
+        var valueToChunk = value
+        while (valueToChunk.length % chunkSize != 0) {
+            valueToChunk = "0$valueToChunk"
+        }
+
+        val chunks = valueToChunk.chunked(chunkSize)
+
+        var result = ""
+        chunks.forEach { chunk ->
+            val chunkSum = getSumOfCharSequence(sequence = chunk)
+            result += chunkSum
+        }
+
+        return NumberBaseSystem(result, targetBase)
+    }
+
+    private fun getSumOfCharSequence(sequence: String): Int {
+        var result = 0
+        sequence.reversed().forEachIndexed { index, char ->
             val digit = if (char.isDigit()) char - '0' else char - '7'
             val multiplier = (base.toDouble().pow(index)).toInt()
             result += digit * multiplier
         }
-
-        return Decimal(result)
-    }
-
-    fun convertFromBaseToBase(targetBase: Int) : NumberBaseSystem {
-        val decimal = convertFromBaseToDecimal()
-        return decimal.convertToBase(targetBase)
+        return result
     }
 }
